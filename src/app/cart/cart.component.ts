63d3c21c1c6fca8ee23a8cb4e593a2546ecf341b
MadhumithaBalaji12 <madhumitha.balaji@impelox.com>
2024-09-17 15:52:54 +0530

    Delete src/app/cart directory

--- src/app/cart/cart.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cart',
-  template: '<h1>Add items here</h1>',
-  styles: ['h1 { color: blue; }']
-})
-export class CartComponent { }
